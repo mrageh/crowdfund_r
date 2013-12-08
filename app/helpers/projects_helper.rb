2f3fc2af6c0e510eb5be8dbd5aa426e2771cf095
Adam89 <Adamm1@aston.ac.uk>
2013-12-08 14:04:28 -0700

    created model test and did a lot of work

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def format_project_pending(project)
+    if project.pledging_expired?
+      content_tag(:strong, "All Done!")
+    else
+      distance_of_time_in_words(Date.today, project.pledging_ends_on) + " remaining"
+    end
+  end
 end
--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -2,13 +2,30 @@ require "spec_helper"
 
 describe "Viewing a individual project" do
   it "shows a project's details" do
-    project = Project.create(project_attributes(target_pledge_amount: 100))
+    project = Project.create(project_attributes(target_pledge_amount: 100, pledging_ends_on: 1.day.from_now))
 
     visit project_path(project)
 
     expect(page).to have_text(project.name)
     expect(page).to have_text(project.description)
     expect(page).to have_text("$100")
+    expect(page).to have_text("1 day remaining")
     expect(page).to have_text(project.website)
+  end
+
+  it "shows the number of days remaining if the pledging end date is in the future" do
+    project = Project.create(project_attributes(pledging_ends_on: 1.day.from_now))
+
+    visit project_path(project)
+
+    expect(page).to have_text("1 day remaining")
+  end
+
+  it "shows 'All Done!', if the pledging end date is in the past" do
+    project = Project.create(project_attributes(pledging_ends_on: 2.days.ago))
+
+    visit project_path(project)
+
+    expect(page).to have_text("All Done!")
   end
 end
